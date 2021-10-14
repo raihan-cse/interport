e98c22cf59a4b2e31cde16caa318acf56b70bca9
Raihan Kabir <raihancse46@gmail.com>
2021-10-14 12:28:50 +0600

    triners carousel done

--- assets/js/carousel-activation.js
+++ assets/js/carousel-activation.js
@@ -18,5 +18,24 @@ $(document).ready(function () {
       }
     });
 
+    // trainers carousel activation
+    $(".trainers-carousel").owlCarousel({
+      items: 3,
+      margin: 24,
+      loop: true,
+      dots: false,
+      nav: true,
+      navText: ['<img src="assets/images/icon-svg/arrow-left.svg" alt="right arrow">', '<img src="assets/images/icon-svg/arrow-right.svg" alt="right arrow">'],
+      responsive: {
+        0: {
+          items: 1,
+          nav: false,
+        },
+        800: {
+          items: 4,
+        }
+      }
+    });
+
     
   });
