14f9a797dc520115529a0d4777bffd6609be44b6
Raihan Kabir <raihancse46@gmail.com>
2021-10-21 13:56:14 +0600

    program details page large device done

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -15,3 +15,32 @@ $('.icon-wrapper').on('click', function() {
 $('.user-item-toggle').on('click', function() {
   $('.user-items').collapse('toggle');
 }); 
+
+$(document).ready(function () {
+  $('.start-step-btn').on('click', function (e) {
+    e.preventDefault();
+    $('.main-content-box, .start-step-btn').addClass('deactive');
+    $('.box-btn.conditional-btn, .multiple-steps.step-one').addClass('active');
+  })
+
+  $('.submit-application-btn').on('click', function (e) {
+    e.preventDefault();
+    $('.box-btn.conditional-btn').html('applied');
+    $('.form-step-one').addClass('deactive');
+    $('.form-step-two').addClass('active');
+  })
+
+  $('.go-back-btn').on('click', function (e) {
+    e.preventDefault();
+    $('.box-btn.conditional-btn').html('applying');
+    $('.form-step-one').removeClass('deactive');
+    $('.form-step-two').removeClass('active');
+  })
+
+  $('.confirm-payment-btn').on('click', function (e) {
+    e.preventDefault();
+    $('.box-btn.conditional-btn').html('pending').addClass('status-pending');
+    $('.multiple-steps.step-one, .form-step-two').removeClass('active');
+    $('.multiple-steps.step-two').addClass('active');
+  })
+})
